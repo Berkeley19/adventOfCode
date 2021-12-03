4419ca81fbce2567586d0671eb7167a765f53088
Berkeley V-H <berkeleyvh20@gmail.com>
2021-12-03 12:03:21 +0000

    2nd part first day

--- firstDayCode.js
+++ firstDayCode.js
@@ -12,19 +12,34 @@ const collectData = async () => {
 
 const checkDepth = (array) => {
     let endProduct = 0
-    for(let i=1; i<=array.length; i++){
+    let number = 0
+    for(let i=1; i<array.length; i++){
         if(array[i]>array[i-1]){
-            console.log(endProduct)
             endProduct++;
         }
     }
     return endProduct
 }
 
+const checkThreeSum = (array) => {
+    let endProduct = 0
+    let prev = 0
+    for(let i=3; i<array.length; i++){
+        let curr = array[i-2] + array[i-1] + array[i]
+        if(curr>prev){
+            endProduct++
+        }
+        prev = curr
+    }
+    return endProduct
+}
+
 (async () => {
     const finalData = await collectData()
-    const arr = finalData.split('\n')
-    console.log(checkDepth(arr))
+    const arr = finalData.split('\n').map(function(item){
+        return parseInt(item, 10)
+    })
+    console.log(checkThreeSum(arr))
 })()
 
 
