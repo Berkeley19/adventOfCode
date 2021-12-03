8def1ac07317a1931a8aa2c8f1f820ab2a1a90ba
Berkeley V-H <berkeleyvh20@gmail.com>
2021-12-03 13:37:38 +0000

    Day2 part2

--- secondDayCode.js
+++ secondDayCode.js
@@ -21,11 +21,33 @@ const finalDepthMulHorizontal = (array) => {
     return h * d
 }
 
-
+const finalWithAim = (array) => {
+    let h = 0;
+    let a = 0;
+    let d = 0;
+    for(let i=0; i<array.length; i++){
+        let str = array[i]
+        let currNum = Number(str.slice(-1))
+        let currMov = str.charAt(0)
+        switch(currMov){
+            case 'f':
+                h+=currNum;
+                d+=a*currNum;
+                break;
+            case 'd':
+                a+=currNum;
+                break;
+            default:
+                a-=currNum;
+        }
+        console.log('h-', h, '   a-', a, '   d-', d)
+    }
+    return h * d
+}
 
 (async () => {
     const finalData = await collectInputs.getInput('secondDayInput.txt')
     const arr = finalData.split('\n')
 
-    console.log(finalDepthMulHorizontal(arr))
+    console.log(finalWithAim(arr))
 })()
